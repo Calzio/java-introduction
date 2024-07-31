b87db0fc0ed23bbc9ece40979d8eece9ca375886
Calzio <162699126+Calzio@users.noreply.github.com>
2024-07-31 11:39:15 -0300

    Add files via upload

---
+++ NetBeansProjects/Hassum/src/main/java/com/mycompany/hassum/Hassum.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hassum;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Hassum {
+
+    public static void main(String[] args) {
+    //Declaração de variavéis
+    String nome = "Marta";
+    int idade = 30;
+    double peso = 65.2;
+    
+    //Declaração de constantes
+    final String CPF = "123.456.789.00";
+    
+    //Concatenando com o sinal de "+"
+    System.out.println("O nome do usuário é: " + nome);
+    System.out.println("O CPF do usuário é: " + CPF );
+    System.out.println("A idade do usuário é: " + idade + " anos");
+    System.out.println("O peso do usuário é: " + peso + " quilos");
+    }
+}
---
+++ NetBeansProjects/Operadores/src/main/java/com/mycompany/operadores/Operadores.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadores;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Operadores {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        
+        //Operadores aritméticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+        
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+    }
+}
